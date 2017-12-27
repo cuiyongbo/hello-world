60b8ce4f47d219724f9550d4530483bc74274220
cuiyb <cuiyb@mapbar.com>
2017-12-27 13:48:58 +0800

    Add git-branch-by-commit-date.sh

---
+++ git-branches-by-commit-date.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/sh
+
+# Credit http://stackoverflow.com/a/2514279
+
+for branch in `git branch -r | grep -v HEAD`
+do
+	echo -e `git show --format="%ai %ar by %an" $branch | head -n 1` \\t$branch
+done |
+	sort -r
