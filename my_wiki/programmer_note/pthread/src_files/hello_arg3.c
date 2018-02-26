2c7cf7c368aaea4c4ad9215350f3f8182d4bddd1
cuiyb <cuiyb@mapbar.com>
2018-02-26 14:55:42 +0800

    Added examples of passing pthread_creat() arguments

--- my_wiki/programmer_note/pthread/src_files/hello_arg3.c
+++ my_wiki/programmer_note/pthread/src_files/hello_arg3.c
@@ -7,34 +7,31 @@
 * AUTHOR: Blaise Barney
 * LAST REVISED: 07/16/14
 ******************************************************************************/
-#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #define NUM_THREADS     8
 
 void *PrintHello(void *threadid)
 {
-   long taskid;
    sleep(1);
-   taskid = *(long *)threadid;
+   long taskid = *(long *)threadid;
    printf("Hello from thread %ld\n", taskid);
    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-pthread_t threads[NUM_THREADS];
-int rc;
-long t;
-
-for(t=0;t<NUM_THREADS;t++) {
-  printf("Creating thread %ld\n", t);
-  rc = pthread_create(&threads[t], NULL, PrintHello, (void *) &t);
-  if (rc) {
-    printf("ERROR; return code from pthread_create() is %d\n", rc);
-    exit(-1);
+  pthread_t threads[NUM_THREADS];
+  for(long t=0;t<NUM_THREADS;t++) 
+  {
+    printf("Creating thread %ld\n", t);
+    int rc = pthread_create(&threads[t], NULL, PrintHello, (void *) &t);
+    if (rc) 
+    {
+      printf("ERROR; return code from pthread_create() is %d\n", rc);
+      exit(-1);
     }
-   }
-
-pthread_exit(NULL);
+  }
+  pthread_exit(NULL);
 }
